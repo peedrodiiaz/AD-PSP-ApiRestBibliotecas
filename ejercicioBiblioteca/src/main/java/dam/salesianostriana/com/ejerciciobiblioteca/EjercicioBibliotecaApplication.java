c29cb67a5773f2b7848123d927df5a9ba700b747
Pedro <diaz.gaped24@triana.salesianos.edu>
2025-11-19 13:32:40 +0100

    subida

---
+++ ejercicioBiblioteca/src/main/java/dam/salesianostriana/com/ejerciciobiblioteca/EjercicioBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package dam.salesianostriana.com.ejerciciobiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioBibliotecaApplication.class, args);
+	}
+
+}
---
+++ ejercicioBiblioteca/src/test/java/dam/salesianostriana/com/ejerciciobiblioteca/EjercicioBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package dam.salesianostriana.com.ejerciciobiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
